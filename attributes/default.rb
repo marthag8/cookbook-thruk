a2b407c33ce98d925a44901b95d4a3b9d53bbe23
Dmitry <dmitry.r@cyberflow.net>
2014-08-28 20:08:07 +0300

    issue #3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,25 +1,27 @@
 
-default["thruk"]["version"] = "1.72-2"
+default['thruk']['version'] = '1.72-2'
 
-default["thruk"]["dir"] = "/usr/share/thruk"
-default["thruk"]["docroot"] = "/usr/share/thruk/root/thruk"
-default["thruk"]["log_dir"] = "/var/log/thruk"
-default["thruk"]["conf_dir"] = "/etc/thruk"
-default["thruk"]["htpasswd"] = "/etc/thruk/htpasswd"
-default["thruk"]["use_ssl"] = false
+default['thruk']['packages'] = %w{libcairo2 libcurl3 libfontconfig1 libfreetype6 libgd2-xpm libjpeg62 libmysqlclient16 libpng12-0 libxpm4 xvfb}
 
-default["thruk"]["cert_name"] = node["fqdn"]
-default["thruk"]["cgi"]["admin_group"] = "admins"
-default["thruk"]["cgi"]["read_groups"] = "all"
+default['thruk']['dir'] = '/usr/share/thruk'
+default['thruk']['docroot'] = '/usr/share/thruk/root/thruk'
+default['thruk']['log_dir'] = '/var/log/thruk'
+default['thruk']['conf_dir'] = '/etc/thruk'
+default['thruk']['htpasswd'] = '/etc/thruk/htpasswd'
+default['thruk']['use_ssl'] = false
+
+default['thruk']['cert_name'] = node['fqdn']
+default['thruk']['cgi']['admin_group'] = 'admins'
+default['thruk']['cgi']['read_groups'] = 'all'
 
 
 default['thruk']['first_day_of_week'] = 1
-default['thruk']['start_page'] = "/thruk/main.html"
+default['thruk']['start_page'] = '/thruk/main.html'
 
 default['thruk']['cmd_defaults']['ahas'] = 0
 default['thruk']['cmd_defaults']['broadcast_notification'] = 0
 default['thruk']['cmd_defaults']['force_check'] = 0
-default['thruk']['cmd_defaults']['force_notification'] = 0 
+default['thruk']['cmd_defaults']['force_notification'] = 0
 default['thruk']['cmd_defaults']['send_notification'] = 1
 default['thruk']['cmd_defaults']['sticky_ack'] = 1
 default['thruk']['cmd_defaults']['persistent_comments'] = 1
