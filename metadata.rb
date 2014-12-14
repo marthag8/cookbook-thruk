802398a9dbce215535c25c6bdd121b76e64c0491
Martha Greenberg <marthag@techfinancials.com>
2014-12-14 21:11:03 +0200

    version bump and fix maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'thruk'
-maintainer       'Tech Financials'
-maintainer_email 'marthag@techfinancials.com'
+maintainer       'Martha Greenberg'
+maintainer_email 'marthag@mit.edu'
 license          'Apache 2.0'
 description      'Installs/Configures thruk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.4'
+version          '0.3.0'
 
 depends "yum-epel"
 depends "apache2"
