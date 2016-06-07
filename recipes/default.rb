b5dab104e468820917c56bfb5c9bb61d9750b24f
Dmitry <dmitry.r@cyberflow.net>
2016-06-07 15:16:51 +0300

    remove mod_php5

--- recipes/default.rb
+++ recipes/default.rb
@@ -28,7 +28,7 @@ node.set['apache']['mpm'] = 'prefork' if node['apache']['mpm'] == 'event'
 
 include_recipe 'apache2'
 include_recipe 'apache2::mod_rewrite'
-include_recipe 'apache2::mod_php5'
+# include_recipe 'apache2::mod_php5'
 include_recipe 'apache2::mod_fcgid'
 include_recipe 'apache2::mod_ssl' if node['thruk']['use_ssl']
 
